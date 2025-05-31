b56fd049e01038c90df723c1806a6668dab782c8
cryskram <vageeshgn2005@gmail.com>
2025-05-31 23:05:15 +0530

    basic ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const wdxl = localFont({ src: "./fonts/wdxl.ttf" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${wdxl.className} bg-background text-foreground`}>
         {children}
       </body>
     </html>
